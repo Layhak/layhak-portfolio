e03cb81af7b295dc06df1c9808264d1b7e33703c
Layhak <layhakheng@gmail.com>
2024-08-25 00:44:11 +0700

    change button style

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { useLanguage } from "@/config/languageContext";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import { motion } from "framer-motion";
 import { FlipWords } from "@/components/ui/flip-words";
-import { Button } from "@/components/ui/button";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
 
 const translations = {
@@ -12,13 +11,22 @@ const translations = {
     name: "Layhak",
     buttonText: "Contact me now",
     words: ["Modern", "Clean", "Responsive"],
-    splitWords: ["Website", "With", "Layhak"],
+    splitWords: [
+      { text: "With" },
+      { text: "Layhak", className: "text-primary font-bold dark:text-primary" },
+    ],
   },
   kh: {
     name: "ឡាយហាក់",
     buttonText: "ទំនាក់ទំនងមកខ្ញុំឥឡូវនេះ",
     words: ["ទំនើប", "ស្អាត", "ឆ្លាតវៃ"],
-    splitWords: ["ជាមួយ", "ឡាយហាក់"],
+    splitWords: [
+      { text: "ជាមួយ" },
+      {
+        text: "ឡាយហាក់",
+        className: "text-primary font-bold dark:text-primary",
+      },
+    ],
   },
 };
 
@@ -26,7 +34,7 @@ export default function Home() {
   const { language } = useLanguage();
   const content = translations[language];
   const words = content.words;
-  const splitWords = content.splitWords.map((word) => ({ text: word }));
+  const splitWords = content.splitWords;
 
   return (
     <AuroraBackground>
@@ -43,18 +51,51 @@ export default function Home() {
         <div className="h-[40rem] flex justify-center items-center flex-col px-4">
           <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
             {language === "en" ? (
-              "Build"
+              <>Build</>
             ) : (
               <>
                 បង្កើត <span className="font-bold text-primary ">គេហទំព័រ</span>
               </>
             )}
             <FlipWords words={words} language={language} duration={3000} />
-            <br />
-            <TypewriterEffectSmooth words={splitWords} language={language} />
-            <Button className="bg-natural-50 dark:bg-neutral-950 rounded-full w-fit dark:text-neutral-50 text-neutral-950 px-4 py-2">
-              {content.buttonText}
-            </Button>
+            {language === "en" ? (
+              <>
+                <span className="text-primary font-bold dark:text-primary">
+                  Website
+                </span>
+              </>
+            ) : (
+              ""
+            )}
+            <TypewriterEffectSmooth
+              className={"text-neutral-600 dark:text-neutral-400"}
+              words={splitWords}
+              language={language}
+            />
+            <button className="bg-white dark:bg-zinc-950 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-zinc-950 dark:text-white inline-block">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(0,112,243,0.6)_0%,rgba(0,112,243,0)_75%)] dark:bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+              </span>
+              <div className="relative flex space-x-2 items-center z-10 rounded-full py-3 px-10 ring-1 ring-zinc-950/20 dark:ring-white/10">
+                <span>{content.buttonText}</span>
+                <svg
+                  fill="none"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  width="16"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.75 8.75L14.25 12L10.75 15.25"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                  />
+                </svg>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-blue-500/0 via-blue-500/90 to-blue-500/0 dark:from-emerald-400/0 dark:via-emerald-400/90 dark:to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+            </button>
           </div>
         </div>
       </motion.section>
--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -16,60 +16,27 @@ export const TypewriterEffectSmooth = ({
   cursorClassName?: string;
   language: string;
 }) => {
-  // Function to determine if the character is non-whitespace
-  const isNonWhitespace = (char: string): boolean => char.trim().length > 0;
-
-  const wordsArray = words.map((word) => {
-    const textArray = word.text.split("");
-    // Reverse iterate to find the last non-whitespace character
-    const lastNonWhitespaceIndex = textArray.reduce(
-      (lastIndex, char, index) => (isNonWhitespace(char) ? index : lastIndex),
-      -1,
-    );
-    return {
-      ...word,
-      text: textArray,
-      lastNonWhitespaceIndex, // Record the last non-whitespace character index
-    };
-  });
+  // split text inside of words into array of characters
+  const wordsArray = words.map((word) => ({
+    ...word,
+    text: word.text.split(""),
+  }));
 
   const renderWords = () => {
     return (
       <div>
         {wordsArray.map((word, idx) => {
-          // Determine if the current word is the next to last word
-          const isNextToLast =
-            language === "kh"
-              ? idx === wordsArray.length - 2
-              : idx === wordsArray.length - 2;
-
           return (
-            <div
-              key={`word-${idx}`}
-              className={cn("inline-block", {
-                "text-primary font-bold": isNextToLast,
-              })}
-            >
-              {word.text.map((char, index) => {
-                // Apply styles to the character if it is the last non-whitespace character
-                const isLastNonWhitespace =
-                  index === word.lastNonWhitespaceIndex;
-                return (
-                  <span
-                    key={`char-${index}`}
-                    className={cn(
-                      "dark:text-white text-black",
-                      word.className,
-                      {
-                        "text-primary font-bold": isLastNonWhitespace,
-                      },
-                    )}
-                  >
-                    {char}
-                  </span>
-                );
-              })}
-              &nbsp;
+            <div key={`word-${idx}`} className="inline-block">
+              {word.text.map((char, index) => (
+                <span
+                  key={`char-${index}`}
+                  className={cn(`dark:text-white text-black`, word.className)}
+                >
+                  {char}
+                </span>
+              ))}
+              &nbsp;{/* Ensure the space is not styled */}
             </div>
           );
         })}
@@ -81,12 +48,8 @@ export const TypewriterEffectSmooth = ({
     <div className={cn("flex space-x-1 my-6", className)}>
       <motion.div
         className="overflow-hidden pb-2"
-        initial={{
-          width: "0%",
-        }}
-        whileInView={{
-          width: "fit-content",
-        }}
+        initial={{ width: "0%" }}
+        whileInView={{ width: "fit-content" }}
         transition={{
           duration: 2,
           ease: "linear",
@@ -95,20 +58,14 @@ export const TypewriterEffectSmooth = ({
       >
         <div
           className="text-xs sm:text-base md:text-xl lg:text-3xl xl:text-5xl font-bold"
-          style={{
-            whiteSpace: "nowrap",
-          }}
+          style={{ whiteSpace: "nowrap" }}
         >
           {renderWords()}{" "}
         </div>{" "}
       </motion.div>
       <motion.span
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{
           duration: 0.8,
           repeat: Infinity,
